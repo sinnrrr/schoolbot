01520ee1d37b154f69e69ee830434798db4654bf
sinnrrr <dimasoltusyuk@gmail.com>
2020-09-28 15:42:36 +0300

    init

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,78 @@
 #[macro_use]
 extern crate dotenv_codegen;
 
-use actix_web::{get, HttpServer, App, Responder, HttpResponse};
 use dotenv::dotenv;
+use teloxide::{dispatching::update_listeners, prelude::*};
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world")
+use std::{convert::Infallible, env, net::SocketAddr};
+use tokio::sync::mpsc;
+use warp::Filter;
+
+use reqwest::StatusCode;
+
+#[tokio::main]
+async fn main() {
+    run().await;
 }
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
+async fn run() {
     dotenv().ok();
 
-    HttpServer::new(|| {
-        App::new()
-            .service(hello)
-    })
-        .bind(format!(
-            "{}:{}",
-            dotenv!("ACTIX_HOST"),
-            dotenv!("ACTIX_PORT")
-        ))?
-        .run()
-        .await
+    teloxide::enable_logging!();
+    log::info!("Starting bot...");
+
+    let bot = Bot::from_env();
+
+    let cloned_bot = bot.clone();
+    teloxide::repl_with_listener(
+        bot,
+        |message| async move {
+            message.answer_dice().send().await?;
+            ResponseResult::<()>::Ok(())
+        },
+        webhook(cloned_bot).await,
+    ).await;
+}
+
+async fn handle_rejection(error: warp::Rejection) -> Result<impl warp::Reply, Infallible> {
+    log::error!("Cannot process the request due to: {:?}", error);
+    Ok(StatusCode::IM_A_TEAPOT)
+}
+
+pub async fn webhook<'a>(bot: Bot) -> impl update_listeners::UpdateListener<Infallible> {
+    let url = format!("https://{}/bot{}", dotenv!("HOST"), dotenv!("TELOXIDE_TOKEN"));
+
+    bot.set_webhook(url).send().await.expect("Cannot setup a webhook");
+
+    let (tx, rx) = mpsc::unbounded_channel();
+
+    let server = warp::post()
+        .and(warp::path(format!("bot{}", dotenv!("TELOXIDE_TOKEN"))))
+        .and(warp::body::json())
+        .map(move |json: serde_json::Value| {
+            let try_parse = match serde_json::from_str(&json.to_string()) {
+                Ok(update) => Ok(update),
+                Err(error) => {
+                    log::error!(
+                        "Cannot parse an update\nError: {:?}\nValue: {:?}\n",
+                        error,
+                        json
+                    );
+
+                    Err(error)
+                }
+            };
+
+            if let Ok(update) = try_parse {
+                tx.send(Ok(update)).expect("Cannot send an incoming update from webhook")
+            }
+
+            StatusCode::OK
+        }).recover(handle_rejection);
+
+    let serve = warp::serve(server);
+    let address = format!("0.0.0.0:{}", dotenv!("PORT"));
+
+    tokio::spawn(serve.run(address.parse::<SocketAddr>().unwrap()));
+    rx
 }
