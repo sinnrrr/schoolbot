aac215c0e850c6ac1adc7d66e15b94500ac1a4fc
sinnrrr <dimasoltusyuk@gmail.com>
2020-10-17 19:19:03 +0300

    init

--- bot.go
+++ bot.go
@@ -1,6 +1,9 @@
 package main
 
-import tb "gopkg.in/tucnak/telebot.v2"
+import (
+	"github.com/sinnrrr/schoolbot/handlers"
+	tb "gopkg.in/tucnak/telebot.v2"
+)
 
 var (
 	menu = &tb.ReplyMarkup{ResizeReplyKeyboard: true}
@@ -17,23 +20,8 @@ func RegisterMenu(bot *tb.Bot)  {
 		menu.Row(updateButton, deleteButton),
 	)
 
-	bot.Handle(&getButton, getButtonHandler)
-	bot.Handle(&addButton, addButtonHandler)
-	bot.Handle(&updateButton, updateButtonHandler)
-	bot.Handle(&deleteButton, deleteButtonHandler)
-}
-
-func getButtonHandler(m *tb.Message) {
-}
-
-func addButtonHandler(m *tb.Message) {
-
-}
-
-func updateButtonHandler(m *tb.Message) {
-
-}
-
-func deleteButtonHandler(m *tb.Message) {
-
+	bot.Handle(&getButton, handlers.GetButton)
+	bot.Handle(&addButton, handlers.AddButton)
+	bot.Handle(&updateButton, handlers.UpdateButton)
+	bot.Handle(&deleteButton, handlers.DeleteButton)
 }
---
+++ handlers/buttons.go
@@ -0,0 +1,18 @@
+package handlers
+
+import tb "gopkg.in/tucnak/telebot.v2"
+
+func GetButton(m *tb.Message) {
+}
+
+func AddButton(m *tb.Message) {
+
+}
+
+func UpdateButton(m *tb.Message) {
+
+}
+
+func DeleteButton(m *tb.Message) {
+
+}
