fe9dcec3f8d6672e45b5ad785268b358b98bd28f
sinnrrr <dimasoltusyuk@gmail.com>
2020-10-17 20:18:48 +0300

    init

---
+++ main.py
@@ -0,0 +1,26 @@
+"""Bot implementation using Telethon library"""
+
+import os
+
+from dotenv import load_dotenv
+from telethon import TelegramClient, events, utils
+
+load_dotenv()
+
+API_ID = int(os.getenv("APP_ID"))
+API_HASH = os.getenv("APP_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+APP_SESSION = os.getenv("APP_SESSION")
+
+bot = TelegramClient(APP_SESSION, API_ID, API_HASH).start(bot_token=BOT_TOKEN)
+
+
+@bot.on(events.NewMessage(pattern=r'(?i).*\b(hello|hi)\b'))
+async def handler(event):
+    sender = await event.get_sender()
+    name = utils.get_display_name(sender)
+    print(name, 'said', event.text, '!')
+
+
+with bot:
+    bot.run_until_disconnected()
